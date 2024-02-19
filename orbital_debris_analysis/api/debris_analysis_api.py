93b36c7865cf833c81c5d6eedf9b0744765a445f
Connor McNaboe <mcnaboeconnor@gmail.com>
2024-02-18 20:17:43 -0700

    fast-api: elastic beanstalk configuration chantges

--- orbital_debris_analysis/api/debris_analysis_api.py
+++ orbital_debris_analysis/api/debris_analysis_api.py
@@ -7,7 +7,7 @@ from orbital_debris_analysis.csv_parser.csv_parser import csv_to_pandas
 router = APIRouter()
 
 
-@router.get("/threat-report/")
+@router.post("/threat-report/")
 async def read_threat_report(mission: Mission) -> ThreatReport:
     full_data_frame = csv_to_pandas("./orbital_debris_analysis/data/SATCAT.csv")
     extracted_data_frame = extract_analysis_data(data_frame=full_data_frame)
